cffea15a2a235e72132b89a322fb2f17eb295b1e
Matt <mattjserve@gmail.com>
2020-04-12 18:12:29 -0500

    mac to pc

--- src/components/messages/messages.js
+++ src/components/messages/messages.js
@@ -10,6 +10,10 @@ class Messages extends Component {
         }
     }
 
+    refreshPage() {
+      window.location.reload(false);
+    }
+
     componentDidMount() {
         fetch('http://localhost:5000/posts/')
             .then(res => res.json())
@@ -22,8 +26,26 @@ class Messages extends Component {
           <h2>Messages</h2>
           <ul>
               {this.state.messages.map(message => 
-                <li key={message._id}>{message.title} {message.description} {message.date}</li>)}
+                <li key={message._id}>
+                  <div>
+                    <p>
+                      {message.title} <br></br> {message.description} <br></br> {message.date} <br></br>
+                      <h4>Conditional</h4>
+                    </p>
+                    <button>Button</button> 
+                  </div>
+                </li>)}
+    
           </ul>
+          <div>
+              <form  action="http://localhost:5000/posts" method="post">
+                <p><label for="title">Enter title: </label>
+                <input id="title" type="text" name="title" placeholder="title"/></p>
+                <p><label for="description">Enter description : </label>
+                <input id="description" type="text" name="description" placeholder="description"/></p>
+                <input onClick={this.refreshPage} type="submit" value="OK"/>
+            </form>
+          </div>
       </div>
     );
   }
